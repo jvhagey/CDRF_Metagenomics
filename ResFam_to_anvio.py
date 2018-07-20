bfff9be274e148ec9c2bfca9aa49673af12a6c95
Jill V. Hagey <jvhagey@gmail.com>
2018-07-20 09:23:25 -0700

    Update ResFam_to_anvio.py

--- ResFam_to_anvio.py
+++ ResFam_to_anvio.py
@@ -1,7 +1,15 @@
 #importing packages
+import glob,os
 import pandas as pd
+import xlrd
 #making txt file for gene.txt file to run anvi-run-hmm
-df = pd.read_excel("C:/Users/jvhagey/Downloads/180102_resfams_metadata_updated_v1.2.2.xlsx", usecols='A:B,F', header=0)
+#setting working directory, change folder to the location of your tblout files
+#os.chdir("C:/Users/jvhagey/OneDrive - UC Davis/Documents/collaboration/dairy sequencing/Metagenomics/Foam_HMMER/")
+os.chdir("C:/Users/Jill/OneDrive - UC Davis/Documents/collaboration/dairy sequencing/Metagenomics/ResFams")
+#Start at row 3 for the header
+df = pd.read_excel("180102_resfams_metadata_updated_v1.2.2.xlsx", usecols='A:B,F', header=3)
+#Move columns around and rename them
+df = df[['Resfam Family Name', 'ResfamID', 'HMM Source']]
 df.columns = ["gene","accession","hmmsource"]
 #write DataFrame to tab separated file (.csv) with file name and TIGRFAM counts
-df.to_csv('C:/Users/jvhagey/OneDrive - UC Davis/Documents/collaboration/dairy sequencing/Metagenomics/Anvio/Resfam_anvi_genes.txt', sep='\t',index=False)
+df.to_csv('Resfam_anvi_genes.txt', sep='\t',index=False)
