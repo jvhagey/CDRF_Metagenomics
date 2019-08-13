1120046c1474c97a810ed80dee78fc9dee91d5ec
Jill V. Hagey <jvhagey@gmail.com>
2019-08-13 11:08:11 -0700

    Update GK_Count_Frequency.py

--- Functional_Annotation/GhostKOALA/GK_Count_Frequency.py
+++ Functional_Annotation/GhostKOALA/GK_Count_Frequency.py
@@ -1,34 +1,72 @@
+#!/usr/bin/env python
+
+## Jill Hagey
+## University of California, Davis
+## jvhagey@gmail.com
+## https://github.com/jvhagey/
+
 #importing packages
-import feather
+#import feather
 import pandas as pd
 import re
 import glob,os
 import csv
-#setting working directory
-os.chdir("C:/Users/Jill/OneDrive - UC Davis/Documents/collaboration/dairy sequencing/Metagenomics/GhostKOALA")
-#making txt file
-file_ortho = "KeggOrthology_Table1.txt"
-file_userdf = "user_ko_definition_all.txt"
-file_user = "user_ko_all.txt"
-#making dataframes
-df_ortho = pd.read_csv(file_ortho, sep=',', header=0)
-df_userdf = pd.read_csv(file_userdf, sep='\t', header=None)
-df_userdf.columns = ['Query','accession','Definition','Score','Second best','Second Score'] #renaming columns
-df_userdf = df_userdf.drop(columns=['Definition','Score','Second best','Second Score']) #droping columns
-new = pd.merge(df_userdf, df_ortho, on='accession')
-cat1 = pd.DataFrame(new['Category1'].value_counts())
-cat2 = pd.DataFrame(new['Category2'].value_counts())
-cat3 = pd.DataFrame(new['Category3'].value_counts())
-cat1.reset_index(inplace=True)
-cat2.reset_index(inplace=True)
-cat3.reset_index(inplace=True)
-cat1.columns = ['Category','Count'] #renaming columns
-cat2.columns = ['Category','Count']
-cat3.columns = ['Category','Count']
-cat1['Percent'] =  (cat1['Count']/sum(cat1['Count']))*100 #Add column for percent 
-cat2['Percent'] =  (cat2['Count']/sum(cat2['Count']))*100
-cat3['Percent'] =  (cat3['Count']/sum(cat3['Count']))*100
-# Write to  file for reading from R
-feather.write_dataframe(cat1, "cat1.feather")
-feather.write_dataframe(cat2, "cat2.feather")
-feather.write_dataframe(cat3, "cat3.feather")
+from argparse import ArgumentParser
+
+#Defining function to process command-line arguments
+def parse_cmdline():
+	"""Parse command-line arguments for script."""
+	parser = ArgumentParser(prog="GK_Count_Frequency.py", description="GhostKOALA output files into a KO count to read into R")
+	parser.add_argument("-i", "--indir", dest="indirname", action="store", default=None, required=True, help="Input directory name GhostKOALA output files are found (required)")
+	parser.add_argument("-c", "--count", dest="countoption", action="store", default=None, required=True, help="Select '-c taxa' to return taxa counts or '-c cat' or category counts (required)")
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	#Parse command-line
+	args = parse_cmdline()
+	#Calling in files
+	os.chdir(args.indirname) #change directory
+	#making dataframes
+	#df_ortho = pd.read_csv("KeggOrthology_Table1.txt", sep=',', header=0)
+	df_userdf = pd.read_csv("user_ko_definition_all.txt", sep='\t', header=None)
+	df_taxdf = pd.read_csv("user.out.top_all.txt", sep='\t', header=None)
+	#df_kodf = pd.read_csv("user_ko_all_cleaned.txt", sep='\t', header=None)
+	#adding column names
+	df_userdf.columns = ['Query','accession','Definition','Score','Second best','Second Score'] #renaming columns
+	df_userdf = df_userdf.drop(columns=['Definition','Score','Second best','Second Score']) #dropping columns
+	df_taxdf.columns = ['Query','accession','Kingdom','Taxa','Lower_taxa','TaxaID','Score'] #renaming columns
+	df_taxdf['Query'] = df_taxdf['Query'].map(lambda x: x.lstrip('user:')) #remove "user:" at beginning of each row in column "Query"
+	df_taxdf = df_taxdf.drop(columns=['TaxaID','Score']) #droping columns
+	new = pd.merge(df_userdf, df_taxdf, on='accession')
+	new2 = pd.merge(new, df_taxdf, on='Query')
+	if args.countoption is None:
+		print("You need to pick either taxa or cat for counts")
+	if args.countoption == 'cat':
+		cat1 = pd.DataFrame(new['Category1'].value_counts())
+		cat2 = pd.DataFrame(new['Category2'].value_counts())
+		cat3 = pd.DataFrame(new['Category3'].value_counts())
+		cat1.reset_index(inplace=True)
+		cat2.reset_index(inplace=True)
+		cat3.reset_index(inplace=True)
+		cat1.columns = ['Category','Count'] #renaming columns
+		cat2.columns = ['Category','Count']
+		cat3.columns = ['Category','Count']
+		cat1['Percent'] = (cat1['Count']/sum(cat1['Count']))*100 #Add column for percent 
+		cat2['Percent'] = (cat2['Count']/sum(cat2['Count']))*100
+		cat3['Percent'] = (cat3['Count']/sum(cat3['Count']))*100
+		# Write to	file for reading from R
+		feather.write_dataframe(cat1, "cat1.feather")
+		feather.write_dataframe(cat2, "cat2.feather")
+		feather.write_dataframe(cat3, "cat3.feather")
+	if args.countoption == 'taxa':
+		#pull out columns with nitrogen fixing KOs
+		Nif_KOs = new2[new2['accession_x'].str.match("K02588|K02586|K02591")] #nifHDK
+		print("The Taxa are:")
+		Nif_KOs = pd.DataFrame(Nif_KOs['Taxa'].value_counts())
+		Nif_KOs.columns = ['Taxa','Number of Gene Calls with NifHDK KOs'] #renaming columns
+		Nif_KOs
+		Nif_KOs = pd.DataFrame(Nif_KOs['Lower_taxa'].value_counts())
+		Nif_KOs.columns = ['Taxa','Number of Gene Calls with NifHDK KOs'] #renaming columns
+		Nif_KOs
+	else:
+		print("You need to pick either taxa or cat for counts")
